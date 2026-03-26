a80a6118435bb29d151123a81de16d6eb2535082
shehab097 <shehabsarder97@gmail.com>
2026-03-26 19:57:33 +0600

    Add missing Vite, Tailwind, and React entry point configurations

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+import AppRoutes from "./routes/AppRoutes";
+
+export default function App() {
+    return (
+        <div className="min-h-screen bg-gray-50">
+            <AppRoutes />
+        </div>
+    );
+}
